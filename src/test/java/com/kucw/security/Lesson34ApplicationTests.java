8cf0e34e18b0e25693bbcee02be62e9ee92d4cd6
Asher <95408550+ss556699ss@users.noreply.github.com>
2024-05-08 17:41:49 +0800

    Update Lesson34ApplicationTests.java

--- src/test/java/com/kucw/security/Lesson34ApplicationTests.java
+++ src/test/java/com/kucw/security/Lesson34ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lesson34ApplicationTests {
+class Lesson34ApplicationTests { 
 
     @Test
     void contextLoads() { 
