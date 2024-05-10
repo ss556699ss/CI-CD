bddbdd4e0d275c359c1b875a330d5ab82f0911da
Asher <95408550+ss556699ss@users.noreply.github.com>
2024-05-10 12:14:10 +0800

    Update Lesson34Application.java

--- src/main/java/com/kucw/security/Lesson34Application.java
+++ src/main/java/com/kucw/security/Lesson34Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lesson34Application {
 
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         SpringApplication.run(Lesson34Application.class, args);
     }
 
