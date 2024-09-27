ef44316633de5cc0453f41cf254746456ce9c724
Asher <95408550+ss556699ss@users.noreply.github.com>
2024-09-27 16:45:42 +0800

    Update Lesson34Application.java

--- src/main/java/com/kucw/security/Lesson34Application.java
+++ src/main/java/com/kucw/security/Lesson34Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lesson34Application {
-// JKKMTGUYTUTU
+// JKKMTGUYTUTUSS
     public static void main(String[] args) {
         SpringApplication.run(Lesson34Application.class, args);
     }
