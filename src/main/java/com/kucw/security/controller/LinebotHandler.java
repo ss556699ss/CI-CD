58da82836c19927b67894c826ac27a6db75de5a5
Asher <95408550+ss556699ss@users.noreply.github.com>
2024-09-27 11:38:08 +0800

    Update LinebotHandler.java

--- src/main/java/com/kucw/security/controller/LinebotHandler.java
+++ src/main/java/com/kucw/security/controller/LinebotHandler.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 
 @LineMessageHandler
 public class LinebotHandler {
-    #asFDG
+    #asFDGgfdgdg
     private final Logger log = LoggerFactory.getLogger(LinebotHandler.class); 
 
     @EventMapping
